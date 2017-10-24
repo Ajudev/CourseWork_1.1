94c789710d4ce02c54853f99d490d5e49a913fb8
M00609218 <M00609218@kvmdxlab110pc23.studentmdx.ac>
2017-10-24 16:54:13 +0400

    i need to finish the shapes

---
+++ CourseWork_1.1/src/coursework_1/pkg1/CourseWork_11.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coursework_1.pkg1;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author M00609218
+ */
+public class CourseWork_11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        final LinkedList<Shape> shapes = new LinkedList();
+        
+        shapes.add(new Rectangle("Rectangle one", 10, 8));
+        shapes.add(new Rectangle("Rectangle two", 20, 5));
+        shapes.add(new Circle("Circle one", 10));
+        shapes.add(new Circle("Circle two", 20));
+        shapes.add(new Cylinder("Cylinder", 5, 10));
+        shapes.add(new Sphere("Sphere", 5));
+        
+        for (final Shape shape:shapes){
+            shape.displayDescription();
+            if (shape instanceof Circle){
+                System.out.println("Area: "+shape.getArea());
+                System.out.println("Circumference: "+ shape.getPerimeter());
+                
+            }
+            else if (shape instanceof Rectangle) {
+                System.out.println("Area: "+shape.getArea());
+                System.out.println("Perimeter: "+shape.getPerimeter());
+            }
+            else if (shape instanceof Cylinder){
+                System.out.println("");
+            }
+            else{
+                
+            }
+            System.out.println("");
+        }
+        
+    }
+    
+}
---
+++ CourseWork_1.1/src/coursework_1/pkg1/Shape.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coursework_1.pkg1;
+
+/**
+ *
+ * @author M00609218
+ */
+public abstract class Shape {
+    final String _name;
+    
+    public Shape(String name){
+
+        _name = name;
+    }
+    
+    public abstract double getArea();
+    
+    public abstract double getPerimeter();
+    
+    
+    public void displayDescription(){
+        System.out.println("I'm a shape named "+_name);
+    }
+}
