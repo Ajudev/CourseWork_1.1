f7f359c8e311b5670425105a746a44cde5d6c458
Ajudev <ajudevnair@gmail.com>
2017-11-21 14:12:39 +0400

    Commenting in server class

--- CourseWork_1.1/src/coursework_1/pkg1/Server.java
+++ CourseWork_1.1/src/coursework_1/pkg1/Server.java
@@ -20,16 +20,20 @@ public class Server {
      */
     public static void main(String args[]){
                 try{
+   /* Lines 24 to 28 inatiales a port for the server to listen to and creates a socket to accept the clients 
+   request for connection and creates an ObjectOutputStream for sending objects to client and ObjectInputStream
+   to accept objects from client*/                 
                     int port = 8000;
                     ServerSocket serverSocket = new ServerSocket(port);
                     Socket socket = serverSocket.accept();
                     ObjectOutputStream toClient = new ObjectOutputStream(socket.getOutputStream());
                     ObjectInputStream fromClient = new ObjectInputStream(socket.getInputStream());
                     
-                    
+       //Creating a new arraylist named to store the object send from the client             
                     ArrayList<Object> userinput = new ArrayList<Object>();
+       //While loop is created to keep the server running 
                     while(true){
-                        Shape s = (Shape)fromClient.readObject();
+                        Shape s = (Shape)fromClient.readObject(); //Used to read contents from the shape class
                         userinput.add(s);
                         toClient.writeObject(userinput);
                         toClient.flush();
